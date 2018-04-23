46e73c2abdb0fa5a5b03114ac2d76e95f7fbdc1b
eda <kimmirindel97@gmail.com>
2018-04-24 11:22:48 +1200

    added comonents to app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 import Words from './Words'
 import AddWord from './AddWord'
+import AddImage from './AddImage'
+import Images from './Images'
 
 const App = () => (
   <div className='app-container'>
     <Words />
     <AddWord />
+    <AddImage />
+    <Images />
   </div>
 )
 
